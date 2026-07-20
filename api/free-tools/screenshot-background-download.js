5170cd7d52971e665dd2aeeb2d2745e6119f55fb
JaferF <otapt2011@gmail.com>
2026-07-21 00:31:46 +0300

    Create screenshot-background-download.js

---
+++ api/free-tools/screenshot-background-download.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Accept, X-CSRF-Token');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  // Always succeed
+  res.status(200).json({
+    success: true,
+    usage_info: {
+      remaining_requests: 998,
+      is_authenticated: false
+    }
+  });
+}
